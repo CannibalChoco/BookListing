e026433d404dd8743f94a0c2ce730593b20a5605
CannibalChoco <fudgeblimp@gmail.com>
2017-06-09 13:18:28 +0300

    small cleanups

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,15 +22,14 @@ public class MainActivity extends AppCompatActivity implements
         LoaderCallbacks<List<Book>>{
 
     private static final String LOG_TAG = MainActivity.class.getName();
-
-    private BookAdapter adapter;
     private static final int BOOK_LOADER_ID = 1;
 
-    // sample search
+    // search urls
     private static final String baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
     private String queryUrl;
 
     private LoaderManager loaderManager;
+    private BookAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,33 +42,10 @@ public class MainActivity extends AppCompatActivity implements
 
         // Get a reference to the LoaderManager to interact with loaders
         loaderManager = getLoaderManager();
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        Log.i(LOG_TAG, "TEST: initLoader()");
         loaderManager.initLoader(BOOK_LOADER_ID, null, MainActivity.this);
 
-        //handleIntent(getIntent());
-
     }
 
-/*
-    @Override
-    protected void onNewIntent(Intent intent) {
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-
-            createQueryUrl(query);
-            Log.i(LOG_TAG, "TEST: QUERY in handleIntent: " + queryUrl);
-
-        }
-    }
-*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,24 +57,16 @@ public class MainActivity extends AppCompatActivity implements
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
+        // set onQueryTextListener
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
 
             @Override
             public boolean onQueryTextSubmit(String query) {
-                //Log.i(LOG_TAG, "TEST: Entering onQueryTextSubmit()");
-                //Log.i(LOG_TAG, "TEST: current query: " + query);
-                //Log.i(LOG_TAG, "TEST: current query url: " + queryUrl);
-
                 // create new query url
-                //Log.i(LOG_TAG, "TEST: creating query url");
                 createQueryUrl(query);
-                //Log.i(LOG_TAG, "TEST: url: " + queryUrl);
 
                 // restart loader
-                //Log.i(LOG_TAG, "TEST: restarting Loader");
                 getLoaderManager().restartLoader(BOOK_LOADER_ID, null, MainActivity.this);
-
-
 
                 return true;
             }
@@ -136,7 +103,6 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onLoaderReset(Loader<List<Book>> loader) {
         Log.i(LOG_TAG, "TEST: onLoaderReset()");
-        // loaderManager.destroyLoader(BOOK_LOADER_ID);
         queryUrl = null;
         adapter.clear();
     }
